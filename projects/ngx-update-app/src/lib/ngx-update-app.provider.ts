4a06fdbe91c91512b7ca52bd0d4733335079b2f5
Dominik Hladík <dominik.hladik@seznam.cz>
2023-12-29 18:32:20 +0100

    feat(inject): make inject possible

--- projects/ngx-update-app/src/lib/ngx-update-app.provider.ts
+++ projects/ngx-update-app/src/lib/ngx-update-app.provider.ts
@@ -1,14 +1,23 @@
-import { InjectionToken, Provider } from '@angular/core';
+import { EnvironmentProviders, InjectionToken, makeEnvironmentProviders } from '@angular/core';
 import { NgxUpdateAppOptions } from './ngx-update-app.interface';
 
-export const APP_VERSION_OPTIONS_TOKEN = new InjectionToken<NgxUpdateAppOptions>('[ngxAppVersion] Options');
+export const APP_VERSION_INTERVAL_TOKEN = new InjectionToken<NgxUpdateAppOptions['interval']>(
+  '[ngxAppVersion] Interval'
+);
 
-export const provideUpdateApp = (options: NgxUpdateAppOptions): Provider => {
-  return {
-    provide: APP_VERSION_OPTIONS_TOKEN,
-    useValue: {
-      interval: options.interval,
-      onUpdate: options.onUpdate
+export const APP_VERSION_ON_UPDATE_TOKEN = new InjectionToken<NgxUpdateAppOptions['onUpdate']>(
+  '[ngxAppVersion] On Update'
+);
+
+export const provideUpdateApp = (options: NgxUpdateAppOptions): EnvironmentProviders => {
+  return makeEnvironmentProviders([
+    {
+      provide: APP_VERSION_INTERVAL_TOKEN,
+      useValue: options.interval
+    },
+    {
+      provide: APP_VERSION_ON_UPDATE_TOKEN,
+      useFactory: options.onUpdate
     }
-  };
+  ]);
 };
