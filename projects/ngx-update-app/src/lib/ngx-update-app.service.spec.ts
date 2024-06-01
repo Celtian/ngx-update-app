36ce6a905beee8f0a83cc64296e9bcf2cebbfde8
Dominik Hladík <dominik.hladik@seznam.cz>
2024-06-01 18:12:26 +0200

    fix(tests): tests fixed

--- projects/ngx-update-app/src/lib/ngx-update-app.service.spec.ts
+++ projects/ngx-update-app/src/lib/ngx-update-app.service.spec.ts
@@ -2,17 +2,21 @@ import { TestBed } from '@angular/core/testing';
 
 import { ApplicationRef } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { Subject } from 'rxjs';
 import { provideUpdateApp } from './ngx-update-app.provider';
 import { NgxUpdateAppService } from './ngx-update-app.service';
 
 describe('NgxUpdateAppService', () => {
   let service: NgxUpdateAppService;
   let updateSpy: jasmine.SpyObj<SwUpdate>;
-  let appRefSpy: jasmine.SpyObj<ApplicationRef>;
+  let appRefSpy: jasmine.SpyObj<ApplicationRef> & { afterTick: Subject<void> };
 
   beforeEach(() => {
     updateSpy = jasmine.createSpyObj('SwUpdate', ['checkForUpdate', 'activateUpdate']);
-    appRefSpy = jasmine.createSpyObj('ApplicationRef', ['isStable']);
+    appRefSpy = jasmine.createSpyObj('ApplicationRef', ['isStable']) as jasmine.SpyObj<ApplicationRef> & {
+      afterTick: Subject<void>;
+    };
+    appRefSpy.afterTick = new Subject();
 
     TestBed.configureTestingModule({
       providers: [
